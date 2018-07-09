16df5064a06bda3556502c58e4c9bafc48cc42a5
Jian Lun Wan <jianlunwan@gmail.com>
2018-07-09 16:07:33 +0800

    Initial Commit

---
+++ QinGuanJitHub/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QinGuanJitHub
+//
+//  Created by Wan Jian Lun on 9/7/18.
+//  Copyright © 2018 Wan Jian Lun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
